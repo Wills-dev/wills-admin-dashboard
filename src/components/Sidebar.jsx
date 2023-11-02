3285e19fba443d25549f5b459aa3214bf7f243b4
wills-dev <ejioguvictor6@gmail.com>
2023-11-02 18:59:35 +0100

    update

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -16,10 +16,10 @@ const Sidebar = () => {
     }
   };
 
-  const activeLink = `flex itmes-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2 `;
+  const activeLink = `flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2 `;
 
   const normalLink =
-    "flex itmes-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-gray-700 text-md dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2";
+    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-gray-700 text-md dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2";
 
   return (
     <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10 ">
